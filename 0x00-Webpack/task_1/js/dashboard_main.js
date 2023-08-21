902936116d15cb8e9c271299a37657bf87217079
GideonAmhaG <gideonamha@gmail.com>
2023-08-21 15:37:32 +0300

    pushing other file in public

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,16 +4,14 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button type="button">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<p id=count></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const updateCounter = () => {
-  let times = $('#count').html || 0;
-  $('button').on('click', () => {
-    times++;
-    $('#count').html(`${times}clicks on the button`);
-  });
-};
+$('button').on('click', _.debounce(updateCounter, 1));
 
-_debounce(updateCounter, 500);
-updateCounter();
+let count = 0;
+
+function updateCounter () {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
