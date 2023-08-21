46b2f02aba00b6b225b704ae4decfb03d0b812ae
GideonAmhaG <gideonamha@gmail.com>
2023-08-21 15:29:30 +0300

    test

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,14 +4,16 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button type="button">Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter, 1));
+const updateCounter = () => {
+  let times = $('#count').html || 0;
+  $('button').on('click', () => {
+    times++;
+    $('#count').html(`${times}clicks on the button`);
+  });
+};
 
-let count = 0;
-
-function updateCounter() {
-  count++;
-  $('#count').text(`${count} clicks on the button`);
-}
+_debounce(updateCounter, 500);
+updateCounter();
