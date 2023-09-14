10f65592998d9de0357254581eec7011633124e5
GideonAmhaG <gideonamha@gmail.com>
2023-09-14 12:09:58 +0300

    test

--- 0x06-React_Immutable/0-main.js
+++ 0x06-React_Immutable/0-main.js
@@ -1,10 +1,10 @@
 import getImmutableObject from './0-fromjs';
 
-const object = {
+const obj = {
   fear: true,
   smell: -1033575916.9145899,
   wall: false,
   thing: -914767132
 };
 
-console.log(getImmutableObject(object));
+console.log(getImmutableObject(obj));
