c57ef9c33dde0c7e0c087b100f7838f692c3ecbe
GideonAmhaG <gideonamha@gmail.com>
2023-10-03 13:24:46 +0300

    moded notification action types

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionTypes.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -4,6 +4,8 @@ export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
 
 export const FETCH_NOTIFICATIONS_SUCCESS = 'FETCH_NOTIFICATIONS_SUCCESS';
 
+export const SET_LOADING_STATE = "SET_LOADING_STATE";
+
 export const NotificationTypeFilters = {
   DEFAULT: 'DEFAULT',
   URGENT: 'URGENT'
